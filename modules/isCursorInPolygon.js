43cd10357c2824f6b7cde6c7cc269702d6b811f3
KonstantinUp <StarodubenkoK@yandex.ru>
2018-03-31 22:52:56 +0600

    renamed files

--- modules/isCursorInPolygon.js
+++ modules/isCursorInPolygon.js
@@ -1,15 +1,15 @@
 function inPolygon(polygonCoordinates,mouse){
     var vertexes = polygonCoordinates.length;
     j = vertexes - 1;
-    var c = 0;
+    var result = false;
     for (i = 0; i < vertexes;i++){
         if ((((polygonCoordinates[i].y<=mouse.y ) && (mouse.y<polygonCoordinates[j].y)) || ((polygonCoordinates[j].y<=mouse.y) && (mouse.y<polygonCoordinates[i].y))) &&
             (mouse.x  > (polygonCoordinates[j].x - polygonCoordinates[i].x) * (mouse.y - polygonCoordinates[i].y) / (polygonCoordinates[j].y - polygonCoordinates[i].y) + polygonCoordinates[i].x)) {
-            c = !c
+            result = !result
         }
         j = i;
     }
-    return c;
+    return result;
 }
 
 
